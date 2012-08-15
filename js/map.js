4d4157bacfdb1f72ced8e6b1651787517649bdbf
Christopher Miller <cmiller0189@gmail.com>
2012-08-15 18:51:35 -0500

    Working!

--- js/map.js
+++ js/map.js
@@ -1,79 +1,123 @@
-			
-      var map;
+var map,
+    stops,
+    infowindow,
+    markersArray = [],
+    serviceURL = "http://orangelit.com:8008/stops";
 
-      function initialize() {
-        var mapOptions = {
-          zoom: 6,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        map = new google.maps.Map(document.getElementById('map'),
-            mapOptions);
+function initialize() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 16,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  });
 
-        // Try HTML5 geolocation
-        if(navigator.geolocation) {
-			
-			var serviceURL = "http://orangelit.com:8008/stops";
-			
-          navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = new google.maps.LatLng(position.coords.latitude,
-                                             position.coords.longitude);
-
-            var infowindow = new google.maps.InfoWindow({
-              map: map,
-              position: pos,
-              content: 'Location found using HTML5.'
-            });
-
-            map.setCenter(pos);
-			
-			var stopsList = $.ajax({
-				url: serviceURL,
-				type: "GET",
-				data: { latitude: position.coords.latitude, 
-						longitude: position.coords.longitude }, 
-				dataType: "JSON"
-			})
-			.done(function(returnData){
-				alert("SUCCESS");
-				
-				returnData.stops.each(function(index) {
-					var infowindow2 = new google.maps.InfoWindow(options);
-					var latlng2 = new google.maps.LatLng($(this).latitude, $(this).longitude);
-					createMarker(latlng2, $(this).stopName, $(this).routeDirection, infowindow2);
-				});
-			})
-			.fail(function(){
-				alert("FAILED");
-			})
-			.always(function(){
-				alert("COMPLETED");
-			})
-			
-          }, function() {
-            handleNoGeolocation(true);
-          });
-        } else {
-          // Browser doesn't support Geolocation
-          handleNoGeolocation(false);
-        }
-      }
-
-      function handleNoGeolocation(errorFlag) {
-        if (errorFlag) {
-          var content = 'Error: The Geolocation service failed.';
-        } else {
-          var content = 'Error: Your browser doesn\'t support geolocation.';
-        }
-
-        var options = {
-          map: map,
-          position: new google.maps.LatLng(-20, 88),
-          content: content
-        };
-
-        var infowindow = new google.maps.InfoWindow(options);
-        map.setCenter(options.position);
-      }
-
-      google.maps.event.addDomListener(window, 'load', initialize);
+  if (navigator.geolocation) {
+  
+    navigator.geolocation.getCurrentPosition(function (position) {
     
+      var pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+      // Define a new infowindow for all markers to use.
+      infowindow = new google.maps.InfoWindow({
+        map: map
+      });
+      
+      // Set your location marker.
+      var marker = new google.maps.Marker({
+        map: map,
+        position: pos,
+        icon: "../img/marker.png"
+      });
+
+      map.setCenter(pos);
+
+      // Get the stops around you.
+      $.ajax({
+        url: serviceURL,
+        type: "GET",
+        dataType: "JSON",
+        data: {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude
+        }
+      }).success(function (returnData) {
+      
+        console.log("Request succeeded");
+        stops = returnData.stops;
+        renderSelectedStops();
+        
+      }).error(function () {
+      
+        console.log("Request failed");
+        
+      });
+      
+    }, function () {
+    
+      handleNoGeolocation(true);
+      
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleNoGeolocation(false);
+  }
+}
+
+function renderSelectedStops() {
+
+  var selectedDirection = $("#direction").val().toLowerCase();
+  
+  map.clearMarkers();
+  
+  $.each(stops, function (index) {
+    if (this.routeDirection.toLowerCase() === selectedDirection) {
+      var latlng2 = new google.maps.LatLng(this.latitude, this.longitude);
+      createMarker(latlng2, this.stopName, this.routeDirection, this.arrivalTime, infowindow);
+    }
+  });
+}
+
+function createMarker(latlng, name, address, arrivalTime, infoWindow) {
+  var html = "<b>" + name + "</b> <br />Direction: " + address + "<br />Arrives: " + arrivalTime;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: latlng
+  });
+  markersArray.push(marker);
+  google.maps.event.addListener(marker, 'click', function() {
+    infoWindow.setContent(html);
+    infoWindow.open(map, marker);
+  });
+}
+
+function handleNoGeolocation(errorFlag) {
+
+  var options = {
+    map: map,
+    position: new google.maps.LatLng(-20, 88),
+    content: errorFlag ? 'Error: The Geolocation service failed.' : 'Error: Your browser doesn\'t support geolocation.'
+  };
+
+  var infowindow = new google.maps.InfoWindow(options);
+  map.setCenter(options.position);
+}
+
+google.maps.Map.prototype.clearMarkers = function() {
+  if (markersArray) {
+    for (var i = 0; i < markersArray.length; i++ ) {
+      markersArray[i].setMap(null);
+    }
+  }
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+$(document).ready(function() {
+
+  $("#direction").change(function() {
+    renderSelectedStops();
+  });
+  
+  $("#refreshLocation").click(function() {
+    initialize();
+  });
+});
